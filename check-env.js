3fb1152bc510e108bccb20bb8b0f3a5187c5fb9b
Rosalee Rester <thecatboxer@gmail.com>
2025-08-13 18:53:29 -0700

    root files from local

---
+++ check-env.js
@@ -0,0 +1,2 @@
+console.log('QRNG present:', Boolean(process.env.QRNG_OUTSHIFT_API_KEY), 'len=', (process.env.QRNG_OUTSHIFT_API_KEY||'').length);
+console.log('RANDOM_ORG present:', Boolean(process.env.RANDOM_ORG_API_KEY), 'len=', (process.env.RANDOM_ORG_API_KEY||'').length);
